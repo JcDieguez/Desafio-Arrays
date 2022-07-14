16b98efea24f4b11dbc87c5c12cde125e6276d98
Juan Cruz Dieguez <juancruzdieguez95@gmail.com>
2022-07-14 17:31:24 -0300

    Uso de dom y calculador total de ingresos

--- main.js
+++ main.js
@@ -9,15 +9,27 @@ function capturar() {
     let precioCapturar = document.getElementById("precio").value;
     //console.log("precioCapturar");
 
-
     nuevoProducto = new Articulo(nombreCapturar, precioCapturar);
     //console.log("nuevoProducto");
     agregar();
+    this.calcular_total();
 }
-
+let suma = 0;
 let lista = [];
 function agregar() {
     lista.push(nuevoProducto);
     document.getElementById("tabla").innerHTML += '<tbody><td td>'+nuevoProducto.nombre+'</td><td>'+nuevoProducto.precio+'</td></tbody>';
 };
+
+function calcular_total(){
+    let suma = 0;
+    lista.forEach( producto => {
+        suma = suma + parseInt(producto.precio);
+    })
+    document.getElementById("suma").innerHTML = '$'+suma;
+}
+
+
+
+
 
