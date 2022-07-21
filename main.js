e2ab083fa91f98bc76ad7f67f004edc9ead0d93c
Juan Cruz Dieguez <juancruzdieguez95@gmail.com>
2022-07-21 12:24:29 -0300

    evento de limpiar lista

--- main.js
+++ main.js
@@ -15,21 +15,28 @@ function capturar() {
     this.calcular_total();
 }
 
+// Los datos que voy a introducir en la lista nombre y precio
 let lista = [];
 function agregar() {
     lista.push(nuevoProducto);
-    document.getElementById("tabla").innerHTML += '<tbody><td td>'+nuevoProducto.nombre+'</td><td>'+nuevoProducto.precio+'</td></tbody>';
+    document.getElementById("tabla").innerHTML += '<tbody><td td>' + nuevoProducto.nombre + '</td><td>' + nuevoProducto.precio + '</td></tbody>';
 };
 
-function calcular_total(){
+
+// Calculo el total de los productos ingresados 
+function calcular_total() {
     let suma = 0;
-    lista.forEach( producto => {
+    lista.forEach(producto => {
         suma = suma + parseInt(producto.precio);
     })
-    document.getElementById("suma").innerHTML = '$'+suma;
+    document.getElementById("suma").innerHTML = '$' + suma;
 }
 
 
 
-
-
+// Agregué evento donde limpio toda la tabla mediante un boton
+let borrar = document.getElementById("borrar")
+borrar.addEventListener("click", function() {
+    document.getElementById("tabla").innerHTML ='<thead><tr><th scope="col">Producto</th><th scope="col">Precio</th></tr></thead>';
+    document.getElementById("suma").innerHTML = '$' + 0;
+})
